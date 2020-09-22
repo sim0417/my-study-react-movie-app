6a59b3e4ec12b08c3e4c508b8be5bd971da30799
Eon Jo <34697086+sim0417@users.noreply.github.com>
2020-09-22 22:15:58 +0900

    update routes

---
+++ src/routes/Home.js
@@ -0,0 +1,56 @@
+import React from "react";
+import axios from "axios";
+import MovieItem from "../components/MovieItem";
+import "./Home.css";
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // console.log("data : ", movies);
+    this.setState({ isLoading: false, movies });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader-text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie, index) => {
+              return (
+                <MovieItem
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
